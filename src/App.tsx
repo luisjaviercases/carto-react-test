76ae7fe94bd88ace18151dcc0d69947c13a3514e
Luis Javier Cases <l.cases@cathedralsw.com>
2024-01-23 19:12:50 +0100

    feat(app): add map in main layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import '@/App.css';
 import Sidebar from '@/components/Sidebar/Sidebar';
 import PageContainer from '@/components/PageContainer/PageContainer';
 import ToggleButton from '@/components/ToggleButton/ToggleButton';
+import Map from './components/Map/Map';
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,7 +19,7 @@ function App() {
       </Sidebar>
       <PageContainer isOpen={isOpen}>
         <ToggleButton isOpen={isOpen} onClick={toggleSidebar} />
-        <p>Contenido principal</p>
+        <Map />
       </PageContainer>
     </div>
   );
