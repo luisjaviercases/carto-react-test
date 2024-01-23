925cf5054156efa93782897354cb3af0214345e2
Luis Javier Cases <l.cases@cathedralsw.com>
2024-01-23 18:10:22 +0100

    feat(app): create main layout and add necessary components to show correctly

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import './App.css';
+import { useState } from 'react';
+import '@/App.css';
+import Sidebar from '@/components/Sidebar/Sidebar';
+import PageContainer from '@/components/PageContainer/PageContainer';
+import ToggleButton from '@/components/ToggleButton/ToggleButton';
 
 function App() {
-  return <span>App</span>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div>
+      <Sidebar isOpen={isOpen}>
+        <span>Sidebar content</span>
+      </Sidebar>
+      <PageContainer isOpen={isOpen}>
+        <ToggleButton isOpen={isOpen} onClick={toggleSidebar} />
+        <p>Contenido principal</p>
+      </PageContainer>
+    </div>
+  );
 }
 
 export default App;
