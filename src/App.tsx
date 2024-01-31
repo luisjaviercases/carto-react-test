1e060b073c6bb888c839f2dedfdfea4ca8bcac79
Luis Javier Cases <l.cases@cathedralsw.com>
2024-01-31 17:35:12 +0100

    feat(app): add layers and new components managing functionality there

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,55 @@ import '@/App.css';
 import Sidebar from '@/components/Sidebar/Sidebar';
 import PageContainer from '@/components/PageContainer/PageContainer';
 import ToggleButton from '@/components/ToggleButton/ToggleButton';
-import Map from './components/Map/Map';
+import Map from '@/components/Map/Map';
+import { CartoLayer, MAP_TYPES } from '@deck.gl/carto/typed';
+import LayerList from '@/components/LayerList/LayerList';
+import { useLayerContext } from '@/context/LayerContext';
+import LayerTools from '@/components/LayerTools/LayerTools';
 
 function App() {
+  const { layerStates } = useLayerContext();
   const [isOpen, setIsOpen] = useState(false);
+  const [selectedLayer, setSelectedLayer] = useState<string | null>(null);
+
+  const retailLayer = new CartoLayer({
+    id: 'retails',
+    connection: 'carto_dw',
+    type: MAP_TYPES.TABLE,
+    data: 'carto-demo-data.demo_tables.retail_stores',
+    pointRadiusMinPixels: 1,
+    getLineColor: [0, 0, 0, 0.75],
+    visible: layerStates['retails']?.visible ?? true,
+  });
+
+  const sociodemographicLayer = new CartoLayer({
+    id: 'sociodemographic',
+    connection: 'carto_dw',
+    type: MAP_TYPES.TILESET,
+    data: 'carto-demo-data.demo_tilesets.sociodemographics_usa_blockgroup',
+    getFillColor: [238, 77, 90],
+    visible: layerStates['sociodemographic']?.visible ?? true,
+  });
 
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
   };
 
+  const onLayerClick = (layerId: string) => {
+    selectedLayer === layerId ? setSelectedLayer(null) : setSelectedLayer(layerId);
+  };
+
+  const layers = [retailLayer, sociodemographicLayer];
+
   return (
     <div>
       <Sidebar isOpen={isOpen}>
-        <span>Sidebar content</span>
+        <LayerList layers={layers} onLayerClick={onLayerClick} />
+        {selectedLayer !== null && <LayerTools layerId={selectedLayer} />}
       </Sidebar>
       <PageContainer isOpen={isOpen}>
         <ToggleButton isOpen={isOpen} onClick={toggleSidebar} />
-        <Map />
+        <Map layers={layers} />
       </PageContainer>
     </div>
   );
