a47c48ba118dee1d31865f0991a0c8f5017d0cfa
Luis Javier Cases <l.cases@cathedralsw.com>
2024-01-31 17:34:47 +0100

    feat(map): add layers as props

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import DeckGL from '@deck.gl/react/typed';
 import StaticMap from 'react-map-gl';
-import { CartoLayer, setDefaultCredentials, MAP_TYPES, BASEMAP } from '@deck.gl/carto/typed';
+import { CartoLayer, setDefaultCredentials, BASEMAP } from '@deck.gl/carto/typed';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 const INITIAL_VIEW_STATE = {
@@ -12,31 +12,18 @@ const INITIAL_VIEW_STATE = {
   bearing: 0,
 };
 
-const MapContainer: FC = () => {
+interface MapProps {
+  layers: CartoLayer[];
+}
+
+const MapContainer: FC<MapProps> = ({ layers }) => {
   setDefaultCredentials({
     accessToken: import.meta.env.VITE_CARTO_ACCESS_TOKEN,
     apiBaseUrl: 'https://gcp-europe-west1.api.carto.com',
   });
 
-  const retailLayer = new CartoLayer({
-    id: 'retails',
-    connection: 'carto_dw',
-    type: MAP_TYPES.TABLE,
-    data: 'carto-demo-data.demo_tables.retail_stores',
-    pointRadiusMinPixels: 7,
-    getLineColor: [0, 0, 0, 0.75],
-  });
-
-  const sociodemographicLayer = new CartoLayer({
-    id: 'sociodemographic',
-    connection: 'carto_dw',
-    type: MAP_TYPES.TILESET,
-    data: 'carto-demo-data.demo_tilesets.sociodemographics_usa_blockgroup',
-    getFillColor: [238, 77, 90],
-  });
-
   return (
-    <DeckGL initialViewState={INITIAL_VIEW_STATE} layers={[retailLayer, sociodemographicLayer]} controller={true}>
+    <DeckGL initialViewState={INITIAL_VIEW_STATE} layers={layers} controller={true}>
       <StaticMap mapStyle={BASEMAP.VOYAGER} />
     </DeckGL>
   );
