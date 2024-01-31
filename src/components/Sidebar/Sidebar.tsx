cc7473ee69e1ac43738e394ac974f189c78cffc9
Luis Javier Cases <l.cases@cathedralsw.com>
2024-01-31 17:33:56 +0100

    style(sidebar): add display flex to component

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -9,6 +9,8 @@ interface SidebarProps {
 const StyledSidebar = styled.div<{ $isOpen: boolean }>`
   background-color: #e9e9e9;
   bottom: 0;
+  display: flex;
+  flex-direction: column;
   left: ${({ $isOpen }) => ($isOpen ? '0' : '-282px')};
   padding: 16px;
   position: fixed;
